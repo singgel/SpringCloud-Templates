12b7e8d4fde982448adb6acec47198b3ae6a6d22
hekuangsheng <hekuangsheng@xueqiu.com>
2019-04-09 16:37:32 +0800

    APM监控依赖于ELK

---
+++ spring-cloud-sleuth-zipkin-elk/customer-service/src/main/java/pl/piomin/microservices/customer/model/Account.java
@@ -0,0 +1,39 @@
+package pl.piomin.microservices.customer.model;
+
+public class Account {
+
+	private Integer id;
+	private String number;
+
+	public Account() {
+		
+	}
+
+	public Account(Integer id, String number) {
+		super();
+		this.id = id;
+		this.number = number;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNumber() {
+		return number;
+	}
+
+	public void setNumber(String number) {
+		this.number = number;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("Account [id=%d, number='%s']", id, number);
+	}
+
+}
